7a2c54b8150f2de4bd99f0a698f006de38bed920
premsnow <156342432+premsnow@users.noreply.github.com>
2024-01-11 21:44:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-streamlit.title('My Parent\'s New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
+streamlit.header('Build your own fruit smoothie')
